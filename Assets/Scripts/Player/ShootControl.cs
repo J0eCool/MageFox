8cb5b4bc0441dc03fd6ad1eac09ca57d4944c48e
J0eCool <count.j0ecool@gmail.com>
2016-02-11 17:38:33 -0800

    Able to shoot more than one type of thing

--- Assets/Scripts/Player/ShootControl.cs
+++ Assets/Scripts/Player/ShootControl.cs
@@ -3,18 +3,45 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class ShootControl : JComponent {
-	[SerializeField] private GameObject _bulletPrefab = null;
+	[SerializeField] private ShotData _fireData;
+	[SerializeField] private ShotData _spellData;
 	[SerializeField] private GameObject _shotPoint = null;
 
+	private Dictionary<string, ShotData> _guns = new Dictionary<string, ShotData>();
+
 	protected override void OnStart() {
+		_guns[ButtonManager.FireButton] = _fireData;
+		_guns[ButtonManager.SpellButton1] = _spellData;
+
+		foreach (var gun in _guns.Values) {
+			gun.Init(_shotPoint);
+		}
 	}
 
 	protected override void OnUpdate() {
-		bool didShoot = ButtonManager.Instance.GetButtonDown(ButtonManager.FireButton);
-		if (didShoot) {
-			GameObject bulletObj = Instantiate(_bulletPrefab);
-			Bullet bullet = bulletObj.GetComponent<Bullet>();
-			bullet.Init(_shotPoint.transform.position, transform.forward);
+		foreach (var kv in _guns) {
+			bool didPress = ButtonManager.Instance.GetButtonDown(kv.Key);
+			if (didPress) {
+				kv.Value.DidPress(transform.forward);
+			}
 		}
+	}
+}
+
+[System.Serializable]
+public class ShotData {
+	[SerializeField] private GameObject _prefab;
+
+	private GameObject _shotPoint;
+
+	public void Init(GameObject shotPoint) {
+		_shotPoint = shotPoint;
+	}
+
+	public void DidPress(Vector3 dir) {
+		Vector3 pos = _shotPoint.transform.position;
+		GameObject bulletObj = GameObject.Instantiate(_prefab);
+		Bullet bullet = bulletObj.GetComponent<Bullet>();
+		bullet.Init(pos, dir);
 	}
 }
