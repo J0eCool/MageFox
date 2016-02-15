c32dcc37316971bba11ec2d96f7703975c049d81
J0eCool <count.j0ecool@gmail.com>
2016-02-15 01:23:41 -0800

    Enemies remove themselves after their patterns

---
+++ Assets/Scripts/Enemy/RemoveAfterTime.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RemoveAfterTime : JComponent {
+	[SerializeField] private float _timeToRemove = 10.0f;
+
+	private float _timer = 0.0f;
+
+	protected override void OnUpdate() {
+		_timer += Time.deltaTime;
+		if (_timer >= _timeToRemove) {
+			Remove();
+		}
+	}
+}
